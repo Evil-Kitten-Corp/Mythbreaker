ec01c32a74a5aba162e383b2f471b6d7b3048070
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-04-22 15:13:30 +0100

    Leg IK
    Added more to the procedural walk leg IK in rig -> xleg_target
    
    however the legs aren't moving

--- Assets/IKFootSolver.cs
+++ Assets/IKFootSolver.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Localization.Plugins.XLIFF.V12;
+using UnityEditor.Rendering;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class IKFootSolver : MonoBehaviour
 {
+    Transform body;
+    float footSpacing;
+    public LayerMask terrainLayer;
+
+    private Vector3 newPosition;
+    private Vector3 oldPosition;
+    private float lerp = 0f;
+    private float stepDistance = 0.1f;
+    private float stepHeight = 0.1f;
+    private float speed = 1f;
+    private Vector3 currentPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +29,35 @@ public class IKFootSolver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        transform.position = body.position; 
+
         Ray ray = new Ray(body.position + (body.right * footSpacing), Vector3.down);
-        if (Physics.Raycast(ray,out RaycastHit info 10, terrainLayer.value))
+        if (Physics.Raycast(ray,out RaycastHit info, 10, terrainLayer.value))
         {
-            transform.position = info.point;
+            if (Vector3.Distance(newPosition, info.point) > stepDistance)
+            {
+                lerp = 0;
+                newPosition = info.point;
+            }
         }
+        if (lerp < 1)
+        {
+            Vector3 footPosition = Vector3.Lerp(oldPosition, newPosition, lerp);
+            footPosition.y += Mathf.Sin(lerp * Mathf.PI) * stepHeight;
+
+            currentPosition = footPosition;
+            lerp += Time.deltaTime * speed;
+        }
+        else
+        {
+            oldPosition = newPosition;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(newPosition, 0.5f);
     }
 }
