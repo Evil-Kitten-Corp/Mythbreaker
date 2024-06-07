a080ec5f734ddb35f3319fcabf51a12e5eb101ee
arbyun <115217686+arbyun@users.noreply.github.com>
2024-06-07 22:33:57 +0100

    add feline melee and ranged unit behaviour

---
+++ Assets/FinalScripts/Enemy/RangedEnemyBehaviour.cs
@@ -0,0 +1,143 @@
+using Abilities;
+using UnityEngine;
+
+namespace FinalScripts
+{
+    public class RangedEnemyBehaviour : EnemyBehaviour
+    {
+        public Transform shootPoint;
+        public Bullet bullet;
+        
+        public override void Start()
+        {
+            base.Start();
+            
+            //ENTRY
+            _entryActions.Add(EnemyStates.KNOCKDOWN, () =>
+           {
+               _anim.SetTrigger("KnockUp");  
+           });
+           
+           _entryActions.Add(EnemyStates.IDLE, null);
+           
+           _entryActions.Add(EnemyStates.SEEK, () =>
+           {
+               _anim.SetBool(Walking, true);
+           });
+           
+           _entryActions.Add(EnemyStates.ATTACK, () =>
+           {
+               //aka Shoot
+               transform.LookAt(_target);
+               _anim.SetTrigger(Attack);
+               Shoot(_target);
+               _lastAttackTime = Time.time;
+           });
+           
+           _entryActions.Add(EnemyStates.HIT, () =>
+           {
+               health.Amount -= _dmgToTake;
+               
+               Vector3 randomness = new Vector3(Random.Range(0f, 0.25f), 
+                   Random.Range(0f, 0.25f), Random.Range(0f, 0.25f));
+               
+               DamagePopUpGenerator.current.CreatePopUp(transform.position + randomness, 
+                   _dmgToTake.ToString(), Color.cyan);
+               
+               _anim.SetTrigger(Hit);
+           });
+           
+           _entryActions.Add(EnemyStates.DEATH, () =>
+           {
+               _anim.SetTrigger(Die);
+               health.PassiveDynamics.Enabled = false;
+               TargetingSystem.Instance.screenTargets.Remove(gameObject);
+               OnDeath?.Invoke(gameObject);
+               Destroy(gameObject, _anim.GetCurrentAnimatorClipInfo(0).Length + 5f);
+           });
+           
+           //UPDATE
+           
+           _updateActions.Add(EnemyStates.IDLE, () =>
+           {
+               _target = GameObject.FindWithTag("Player").transform;
+               transform.LookAt(_target);
+           });
+           
+           _updateActions.Add(EnemyStates.SEEK, () =>
+           {
+               Seek(_target.position);
+           });
+           
+           _updateActions.Add(EnemyStates.ATTACK, null);
+           _updateActions.Add(EnemyStates.HIT, null);
+           _updateActions.Add(EnemyStates.DEATH, null);
+           
+           _updateActions.Add(EnemyStates.KNOCKDOWN, () =>
+           {
+               if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Knockdown") | 
+                   _anim.GetCurrentAnimatorStateInfo(0).IsName("GetUp"))
+               {
+                   return;
+               }
+               
+               SwitchState(EnemyStates.IDLE);
+           });
+           
+           //EXIT
+           
+           _exitActions.Add(EnemyStates.IDLE, null);
+           
+           _exitActions.Add(EnemyStates.SEEK, () =>
+           {
+               _anim.SetBool(Walking, false);
+           });
+           
+           _exitActions.Add(EnemyStates.ATTACK, null);
+           _exitActions.Add(EnemyStates.HIT, null);
+           _exitActions.Add(EnemyStates.DEATH, null);
+           _exitActions.Add(EnemyStates.KNOCKDOWN, null);
+           
+           //
+           _entryActions.Add(EnemyStates.PREPARE, null);
+           _updateActions.Add(EnemyStates.PREPARE, null);
+           _exitActions.Add(EnemyStates.PREPARE, null);
+        }
+
+        private void Shoot(Transform target)
+        {
+            Bullet bull = Instantiate(bullet, shootPoint);
+            bull.Spawn(transform.forward, (int)stats.attackDamage, target);
+        }
+
+        public override void Update()
+        {
+            switch (_currentState) 
+            {
+                case EnemyStates.IDLE:
+                    if (_target != null) SwitchState(EnemyStates.SEEK); 
+                    break;
+                case EnemyStates.SEEK:
+                    if (_target == null) SwitchState(EnemyStates.IDLE);
+                    if (Vector3.Distance(transform.position, _target.position) <= stats.range && CanAttack()) 
+                        SwitchState(EnemyStates.ATTACK);
+                    break;
+                case EnemyStates.ATTACK:
+                    SwitchState(EnemyStates.IDLE);
+                    break;
+                case EnemyStates.HIT:
+                    if (health.Amount > 0) SwitchState(EnemyStates.IDLE);
+                    if (health.Amount <= 0) SwitchState(EnemyStates.DEATH);
+                    break;
+                case EnemyStates.PREPARE:
+                    break;
+                case EnemyStates.KNOCKDOWN:
+                    break;
+                case EnemyStates.DEATH:
+                    break;
+            }
+            
+            base.Update();
+        }
+    }
+}
--- Assets/for deletion/EnemyBehaviour.cs
+++ Assets/for deletion/EnemyBehaviour.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
-public class EnemyBehaviour : MonoBehaviour
+public abstract class EnemyBehaviour : MonoBehaviour
 {
     public QuantityBhv health;
     public EnemyStates startingState = EnemyStates.IDLE;
@@ -20,42 +20,43 @@ public class EnemyBehaviour : MonoBehaviour
     public float dissolveRate = 0.0125f;
     public float refreshRate = 0.025f;
 
-    private Material[] _skinnedMats;
+    internal Material[] _skinnedMats;
     
     public Action<float> TakeDamage;
     public Action<GameObject> OnDeath;
     public Action KnockUp;
     
-    private EnemyStates _currentState;
-    private Animator _anim;
-    private NavMeshAgent _agent;
-    private Transform _target;
-    private float _dmgToTake;
-    private float _lastAttackTime; 
+    internal EnemyStates _currentState;
+    internal Animator _anim;
+    internal NavMeshAgent _agent;
+    internal Transform _target;
+    internal float _dmgToTake;
+    internal float _lastAttackTime; 
     
-    private readonly Dictionary<EnemyStates, Action> _updateActions = new();
-    private readonly Dictionary<EnemyStates, Action> _entryActions = new();
-    private readonly Dictionary<EnemyStates, Action> _exitActions = new();
+    internal readonly Dictionary<EnemyStates, Action> _updateActions = new();
+    internal readonly Dictionary<EnemyStates, Action> _entryActions = new();
+    internal readonly Dictionary<EnemyStates, Action> _exitActions = new();
 
-    private static readonly int Walking = Animator.StringToHash("Walking");
-    private static readonly int Attack = Animator.StringToHash("Attack");
-    private static readonly int Hit = Animator.StringToHash("Hit");
-    private static readonly int Die = Animator.StringToHash("Die");
+    internal static readonly int Walking = Animator.StringToHash("Walking");
+    internal static readonly int Attack = Animator.StringToHash("Attack");
+    internal static readonly int Hit = Animator.StringToHash("Hit");
+    internal static readonly int Die = Animator.StringToHash("Die");
     
     public EnemyStats stats;
-    private static readonly int DissolveAmount = Shader.PropertyToID("_DissolveAmount");
+    internal static readonly int DissolveAmount = Shader.PropertyToID("_DissolveAmount");
 
     public enum EnemyStates 
     {
         IDLE, 
-        SEEK, 
+        SEEK,
+        PREPARE,
         ATTACK, 
         HIT, 
         DEATH,
         KNOCKDOWN
     }
 
-    void Start()
+    public virtual void Start()
     {
         _currentState = startingState;
         _anim = GetComponent<Animator>();
@@ -68,97 +69,9 @@ public class EnemyBehaviour : MonoBehaviour
         {
             _skinnedMats = skinnedMeshRenderer.materials;
         }
-        
-        //Hard coding for now, we'll implement this better after testing
-        
-       //ENTRY
-       
-       _entryActions.Add(EnemyStates.KNOCKDOWN, () =>
-       {
-           _anim.SetTrigger("KnockUp");  
-       });
-       
-       _entryActions.Add(EnemyStates.IDLE, null);
-       
-       _entryActions.Add(EnemyStates.SEEK, () =>
-       {
-           _anim.SetBool(Walking, true);
-       });
-       
-       _entryActions.Add(EnemyStates.ATTACK, () =>
-       {
-           transform.LookAt(_target);
-           _anim.SetTrigger(Attack);
-           DealDamage(_target);
-           _lastAttackTime = Time.time;
-       });
-       
-       _entryActions.Add(EnemyStates.HIT, () =>
-       {
-           health.Amount -= _dmgToTake;
-           
-           Vector3 randomness = new Vector3(Random.Range(0f, 0.25f), 
-               Random.Range(0f, 0.25f), Random.Range(0f, 0.25f));
-           
-           DamagePopUpGenerator.current.CreatePopUp(transform.position + randomness, 
-               _dmgToTake.ToString(), Color.cyan);
-           
-           _anim.SetTrigger(Hit);
-       });
-       
-       _entryActions.Add(EnemyStates.DEATH, () =>
-       {
-           _anim.SetTrigger(Die);
-           health.PassiveDynamics.Enabled = false;
-           TargetingSystem.Instance.screenTargets.Remove(gameObject);
-           OnDeath?.Invoke(gameObject);
-           Destroy(gameObject, _anim.GetCurrentAnimatorClipInfo(0).Length + 5f);
-       });
-       
-       //UPDATE
-       
-       _updateActions.Add(EnemyStates.IDLE, () =>
-       {
-           _target = GameObject.FindWithTag("Player").transform;
-           transform.LookAt(_target);
-       });
-       
-       _updateActions.Add(EnemyStates.SEEK, () =>
-       {
-           Seek(_target.position);
-       });
-       
-       _updateActions.Add(EnemyStates.ATTACK, null);
-       _updateActions.Add(EnemyStates.HIT, null);
-       _updateActions.Add(EnemyStates.DEATH, null);
-       
-       _updateActions.Add(EnemyStates.KNOCKDOWN, () =>
-       {
-           if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Knockdown") | 
-               _anim.GetCurrentAnimatorStateInfo(0).IsName("GetUp"))
-           {
-               return;
-           }
-           
-           SwitchState(EnemyStates.IDLE);
-       });
-       
-       //EXIT
-       
-       _exitActions.Add(EnemyStates.IDLE, null);
-       
-       _exitActions.Add(EnemyStates.SEEK, () =>
-       {
-           _anim.SetBool(Walking, false);
-       });
-       
-       _exitActions.Add(EnemyStates.ATTACK, null);
-       _exitActions.Add(EnemyStates.HIT, null);
-       _exitActions.Add(EnemyStates.DEATH, null);
-       _exitActions.Add(EnemyStates.KNOCKDOWN, null);
     }
 
-    private void OnKnockup()
+    internal void OnKnockup()
     {
         if (_currentState == EnemyStates.DEATH)
         {
@@ -168,12 +81,12 @@ public class EnemyBehaviour : MonoBehaviour
         SwitchState(EnemyStates.KNOCKDOWN);
     }
 
-    private void OnDie(GameObject obj)
+    internal void OnDie(GameObject obj)
     {
         StartCoroutine(Dissolve());
     }
 
-    private IEnumerator Dissolve()
+    internal IEnumerator Dissolve()
     {
         if (_skinnedMats.Length > 0)
         {
@@ -193,12 +106,12 @@ public class EnemyBehaviour : MonoBehaviour
         }
     }
 
-    private void Seek(Vector3 location) 
+    internal void Seek(Vector3 location) 
     {
         _agent.SetDestination(location);
     }
 
-    private void OnTakeDamage(float amount)
+    internal void OnTakeDamage(float amount)
     {
         if (_currentState == EnemyStates.DEATH)
         {
@@ -209,7 +122,7 @@ public class EnemyBehaviour : MonoBehaviour
         SwitchState(EnemyStates.HIT);
     }
     
-    private void DealDamage(Transform target)
+    internal virtual void DealDamage(Transform target)
     {
         var atm = target.GetComponent<AttributesManager>();
         
@@ -219,42 +132,19 @@ public class EnemyBehaviour : MonoBehaviour
         }
     }
 
-    void Update()
+    public virtual void Update()
     {
-        switch (_currentState) 
-        {
-            case EnemyStates.IDLE:
-                if (_target != null) SwitchState(EnemyStates.SEEK); 
-                break;
-            case EnemyStates.SEEK:
-                if (_target == null) SwitchState(EnemyStates.IDLE);
-                if (Vector3.Distance(transform.position, _target.position) <= 1 && CanAttack()) 
-                    SwitchState(EnemyStates.ATTACK);
-                break;
-            case EnemyStates.ATTACK:
-                SwitchState(EnemyStates.IDLE);
-                break;
-            case EnemyStates.HIT:
-                if (health.Amount > 0) SwitchState(EnemyStates.IDLE);
-                if (health.Amount <= 0) SwitchState(EnemyStates.DEATH);
-                break;
-            case EnemyStates.KNOCKDOWN:
-                break;
-            case EnemyStates.DEATH:
-                break;
-        }
-        
         _updateActions[_currentState]?.Invoke();
     }
     
-    void SwitchState(EnemyStates state) 
+    internal void SwitchState(EnemyStates state) 
     {
         _exitActions[_currentState]?.Invoke();
         _currentState = state;
         _entryActions[_currentState]?.Invoke();
     }
     
-    private bool CanAttack()
+    internal bool CanAttack()
     {
         return Time.time - _lastAttackTime >= attackCooldown;
     }
