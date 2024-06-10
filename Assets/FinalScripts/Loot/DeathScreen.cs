4b8c1d915d1b33aa01f269cd8f0995a66d050e1d
FranciscoAnjos <fcariaanjos@gmail.com>
2024-06-10 11:34:22 +0100

    fix issue with death screen

--- Assets/FinalScripts/Loot/DeathScreen.cs
+++ Assets/FinalScripts/Loot/DeathScreen.cs
@@ -2,13 +2,23 @@
 
 public class DeathScreen: MonoBehaviour
 {
+    public void OnEnable()
+    {
+        MythUIElement.IsInUI = true; 
+    }
+    
+    public void OnDisable()
+    {
+        MythUIElement.IsInUI = false;
+    }
+
     public void Restart()
     {
-        
+        gameObject.SetActive(false);
     }
 
     public void MainScreen()
     {
-         
+        SceneManager.LoadScene("MainMenu");
     }
 }
